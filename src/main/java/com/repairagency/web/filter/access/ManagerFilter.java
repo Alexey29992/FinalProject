1054870cc5170520cda4a4767643c1bfdb29937c
Alexey29992 <alexey29992@gmail.com>
2021-06-17 12:35:57 +0300

    Fixed Admin access to Manager features

--- src/main/java/com/repairagency/web/filter/access/ManagerFilter.java
+++ src/main/java/com/repairagency/web/filter/access/ManagerFilter.java
@@ -7,11 +7,12 @@ import javax.servlet.annotation.WebFilter;
 @WebFilter("/main/role-dependent/manager/*")
 public class ManagerFilter extends BasicAccessFilter {
 
-    private static final User.Role accessibleRole = User.Role.MANAGER;
+    private static final User.Role accessibleRole1 = User.Role.ADMIN;
+    private static final User.Role accessibleRole2 = User.Role.MANAGER;
 
     @Override
     protected boolean isRoleAppropriate(User.Role role) {
-        return accessibleRole.equals(role);
+        return accessibleRole1.equals(role) || accessibleRole2.equals(role);
     }
 
 }
